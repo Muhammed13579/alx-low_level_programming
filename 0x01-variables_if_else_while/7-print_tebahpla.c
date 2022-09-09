b0ca50288ebb4dd809a3d25b76979d48e4f7353d
Muhammed13579 <salawumuhammed21@gmail.com>
2022-09-09 09:56:58 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i < 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
